11c98269e109f2d272abad336532cabe08c568ad
qiansen <boss.yuan@outlook.com>
2017-01-05 19:52:42 +0800

    增加 添加用户 功能

--- views/forms.py
+++ views/forms.py
@@ -1,11 +1,28 @@
 # coding=utf-8
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, SubmitField, StringField, BooleanField
-from wtforms.validators import DataRequired, Length
+from wtforms import PasswordField, SubmitField, StringField, BooleanField, ValidationError
+from wtforms.validators import DataRequired, Length, EqualTo, Regexp
+from .models import Users
 
 
 class LoginForm(FlaskForm):
-    username = StringField('用户名', validators=[DataRequired(), Length(1, 64)])
-    password = PasswordField('密码', validators=[DataRequired()])
+    username = StringField('用户名', validators=[DataRequired(message='用户名不能为空'), Length(1, 64)])
+    password = PasswordField('密码', validators=[DataRequired(message='请输入密码')])
     remember_me = BooleanField('记住我')
     submit = SubmitField('登录')
+
+    def validate_username(self, field):
+        if not Users.query.filter_by(username=field.data).first():
+            raise ValidationError('用户名不存在')
+
+
+class AddUserForm(FlaskForm):
+    username = StringField('用户名', validators=[
+        DataRequired(message='用户名不能为空'), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, '用户名只能由字母，数字，小数点和下划线组成')])
+    password = PasswordField('密码', validators=[DataRequired(message='密码不能为空'), EqualTo('password2', message='密码不一致')])
+    password2 = PasswordField('确认密码', validators=[DataRequired()])
+    submit = SubmitField('添加')
+
+    def validate_username(self, field):
+        if Users.query.filter_by(username=field.data).first():
+            raise ValidationError('用户名已存在')
--- views/tdl_server.py
+++ views/tdl_server.py
@@ -3,7 +3,7 @@ from flask import render_template, session, redirect, request, flash, url_for
 from . import main, db
 from models import Users, ToDoList
 from flask_login import login_required, current_user, logout_user, login_user
-from .forms import LoginForm
+from .forms import LoginForm, AddUserForm
 
 
 @main.route('/', methods=['POST', 'GET'])
@@ -20,12 +20,12 @@ def api():
 @main.route('/login', methods=['POST', 'GET'])
 def login():
     form = LoginForm()
-    if form.is_submitted():
+    if form.validate_on_submit():
         user = Users.query.filter_by(username=form.username.data).first()
         if user and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
             flash('登陆成功')
-            return redirect(url_for('index'))
+            return redirect(url_for('main.index'))
         flash('登录失败')
     return render_template('login.html', form=form)
 
@@ -35,10 +35,22 @@ def login():
 def logout():
     logout_user()
     flash('已退出')
-    return redirect(url_for('login'))
+    return redirect(url_for('main.login'))
 
 
 @main.route('/addtask')
 @login_required
 def add_task():
     pass
+
+
+@main.route('/adduser', methods=['POST', 'GET'])
+def add_user():
+    form = AddUserForm()
+    if form.validate_on_submit():
+        user = Users(username=form.username.data,
+                     password=form.password.data)
+        db.session.add(user)
+        flash('添加成功')
+        return redirect(url_for('main.login'))
+    return render_template('adduser.html', form=form)
